7e68cf8b9c9ae68f30967f945f084c4bf4846375
shahanazdev19 <shahanazdev19.com>
2021-08-20 20:44:53 +0600

    Add project files.

---
+++ NETCoreWebAPITest/DB/StudentDBContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCoreWebAPITest.DB
+{
+    public class StudentDBContext:DbContext
+    {
+        public StudentDBContext(DbContextOptions<StudentDBContext> options):base(options)
+        {
+
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+}
---
+++ NETCoreWebAPITest/Model/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NETCoreWebAPITest
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string FatherName { get; set; }
+        public string MotherName { get; set; }
+    }
+}
